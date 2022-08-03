68f5b5822cd83e6f74013eabad8b6375c0d3aa4d
Adalberto Gonçalves da Silva <adalberto.goncalves.silva@gmail.com>
2022-08-03 09:32:12 -0300

    Adicionando o typescript e ampliando a forma de desenvolver.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello world!");
+import express, {Request, Response, NextFunction} from 'express';
+
+const app = express();
+const porta = 5000;
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({foo: 'bar'});
+});
+
+app.listen(porta, () => {
+    console.log(`Aplicação executando na porta :`, porta );
+});

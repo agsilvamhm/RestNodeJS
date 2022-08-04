e48b90ceecb3de36d24796dba6a6621e70562796
Adalberto Gonçalves da Silva <adalberto.goncalves.silva@gmail.com>
2022-08-04 03:01:08 -0300

    Commit final com o projeto todo funcionando

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
-import express, {Request, Response, NextFunction} from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import statusRoute from './routes/status.route';
+import usersRoute from './routes/users.route';
 
 const app = express();
 const porta = 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(statusRoute);
+app.use(usersRoute);
 
 app.get('/status', (req: Request, res: Response, next: NextFunction) => {
     res.status(200).send({foo: 'bar'});
